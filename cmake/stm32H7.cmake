43c5c56aaa3c1de7b6dc701b44c3bcbf02ed53ef
Vojtěch Vosáhlo <vosahlov@gmail.com>
2024-07-22 09:27:09 +0200

    Tidy up the cmake filesů

--- cmake/stm32H7.cmake
+++ cmake/stm32H7.cmake
@@ -8,7 +8,7 @@
 # -mfloat-abi=softfp    Allows the generation of code using hardware floating-point instructions, but still uses the soft-float calling conventions.
 
 # adjust 
-set(MCU_OPTS "-mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -mthumb -mabi=aapcs" CACHE INTERNAL "(Target) Architecture options")
+set(MCU_OPTS "-mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -mabi=aapcs" CACHE INTERNAL "(Target) Architecture options")
 set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   ${MCU_OPTS}" CACHE INTERNAL "(Target) C Compiler options")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MCU_OPTS}" CACHE INTERNAL "(Target) C++ Compiler options")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${MCU_OPTS}" CACHE INTERNAL "(Target) ASM Compiler options")
