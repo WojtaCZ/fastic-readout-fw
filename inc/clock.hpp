fc51adf8e7f8891b7b128b3afcca60760e37f54b
Vojtech Vosahlo <vojtech.vosahlo@cern.ch>
2024-10-07 11:54:47 +0200

    Tidied up clock setup

--- inc/clock.hpp
+++ inc/clock.hpp
@@ -1,7 +1,26 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 
+#include <stmcpp/error.hpp>
+
 namespace clock {
+    enum class error {
+        ldo_timeout,
+        vos1_timeout,
+        vos0_timeout,
+        hse_timeout,
+        hsi48_timeout,
+        csi_timeout,
+        flash_latency_timeout,
+        pll1_timeout,
+        domain_source_switch_timeout,
+        clock_compensation_timeout,
+        clock_mux_timeout,
+        other
+    };
+
+    static stmcpp::error::handler<error, "clock"> errorHandler;
+
     void init();
 } 
 
--- src/clock.cpp
+++ src/clock.cpp
@@ -2,86 +2,73 @@
 #include <stmcpp/register.hpp>
 #include <stmcpp/clock.hpp>
 
-uint32_t SystemCoreClock = stmcpp::config::sysclock.toHertz();
-uint32_t tmpReg;
+namespace clock {
+	void init(){
+		// Enable the LDO and check that the voltage levels are valid
+		stmcpp::reg::set(std::ref(PWR->CR3), 1, PWR_CR3_LDOEN);
+		stmcpp::reg::waitForBitSet(std::ref(PWR->CSR1), PWR_CSR1_ACTVOSRDY_Msk, []() { errorHandler.hardThrow(clock::error::ldo_timeout); });
 
-void clock::init(){
-	// Enable the LDO
-	stmcpp::reg::set(std::ref(PWR->CR3), 1, PWR_CR3_LDOEN);
-	// Wait for ready flag
-	while(!stmcpp::reg::read(std::ref(PWR->CSR1), PWR_CSR1_ACTVOSRDY_Msk)){;}
+		// Set voltage scale to 1 and wait for it to be ready
+		stmcpp::reg::set(std::ref(PWR->D3CR), 0b11, PWR_D3CR_VOS_Pos);
+		stmcpp::reg::waitForBitSet(std::ref(PWR->D3CR), PWR_D3CR_VOSRDY_Msk, []() { errorHandler.hardThrow(clock::error::vos1_timeout); });
 
-    //Set voltage scale to 1
-	stmcpp::reg::set(std::ref(PWR->D3CR), 0b11, PWR_D3CR_VOS_Pos);
-	tmpReg = stmcpp::reg::read(std::ref(PWR->D3CR), PWR_D3CR_VOS_Msk);
+		// Enable syscfg clock so that the overdrive can be enabled
+		stmcpp::reg::set(std::ref(RCC->APB4ENR), RCC_APB4ENR_SYSCFGEN);
+		
+		// Enable overdrive (to VOS0) and wait for it to be ready
+		stmcpp::reg::set(std::ref(SYSCFG->PWRCR), SYSCFG_PWRCR_ODEN);
+		stmcpp::reg::waitForBitSet(std::ref(PWR->D3CR), PWR_D3CR_VOSRDY_Msk, []() { errorHandler.hardThrow(clock::error::vos0_timeout); });
 
-	while(!stmcpp::reg::read(std::ref(PWR->D3CR), PWR_D3CR_VOSRDY_Msk)){;}
+		// Enable HSE and CSS and wait for it to be ready
+		stmcpp::reg::set(std::ref(RCC->CR), RCC_CR_CSSHSEON | RCC_CR_HSEON);
+		stmcpp::reg::waitForBitSet(std::ref(RCC->CR), RCC_CR_HSERDY_Msk, []() { errorHandler.hardThrow(clock::error::hse_timeout); });
 
-	stmcpp::reg::set(std::ref(RCC->APB4ENR), RCC_APB4ENR_SYSCFGEN);
+		// Enable HSI48 and wait for it to be ready
+		stmcpp::reg::set(std::ref(RCC->CR), RCC_CR_HSI48ON);
+		stmcpp::reg::waitForBitSet(std::ref(RCC->CR), RCC_CR_HSI48RDY_Msk, []() { errorHandler.hardThrow(clock::error::hsi48_timeout); });
 
-	stmcpp::reg::set(std::ref(SYSCFG->PWRCR), SYSCFG_PWRCR_ODEN);
-	tmpReg = stmcpp::reg::read(std::ref(SYSCFG->PWRCR), SYSCFG_PWRCR_ODEN_Msk);
+		// Enable CSI and wait for it to be ready
+		stmcpp::reg::set(std::ref(RCC->CR), RCC_CR_CSION);
+		stmcpp::reg::waitForBitSet(std::ref(RCC->CR), RCC_CR_CSIRDY_Msk, []() { errorHandler.hardThrow(clock::error::csi_timeout); });
 
-	while(!stmcpp::reg::read(std::ref(PWR->D3CR), PWR_D3CR_VOSRDY_Msk)){;}
-	
+		// Set up the flash latency to work with 480MHz
+		constexpr std::uint32_t FLASH_ACR_CFG = (FLASH_ACR_LATENCY_4WS | (0b11 << FLASH_ACR_WRHIGHFREQ_Pos));
+		stmcpp::reg::write(std::ref(FLASH->ACR), FLASH_ACR_CFG);
+		stmcpp::reg::waitForBitsEqual(std::ref(FLASH->ACR), FLASH_ACR_LATENCY_Msk | FLASH_ACR_WRHIGHFREQ_Msk, FLASH_ACR_CFG, []() { errorHandler.hardThrow(clock::error::flash_latency_timeout); });
 
-	//Set the HSE on and wait for it to be ready
-	stmcpp::reg::set(std::ref(RCC->CR), RCC_CR_CSSHSEON);
-	stmcpp::reg::set(std::ref(RCC->CR), RCC_CR_HSEON);
-	while(!(stmcpp::reg::read(std::ref(RCC->CR)) & RCC_CR_HSERDY_Msk)){;}
+		// Setup the PLL1 (P = 480MHz, Q = 160MHz, R disabled)
+		stmcpp::clock::pll::setSource(stmcpp::clock::pll::clkSource::hse);
+		stmcpp::clock::pll::pll<stmcpp::clock::pll::peripheral::pll1, 3, 60, 2, 6, 0> pll1(stmcpp::clock::pll::inputRange::f8_16MHz);
+		pll1.enable();
+		stmcpp::clock::systick::waitForTrue(pll1.isLocked(), []() { errorHandler.hardThrow(clock::error::pll1_timeout); });
 
-	//Set the HSI48 on and wait for it to be ready
-	stmcpp::reg::set(std::ref(RCC->CR), RCC_CR_HSI48ON);
-	while(!(stmcpp::reg::read(std::ref(RCC->CR)) & RCC_CR_HSI48RDY_Msk)){;}
+		//Setup the domain dividers
+		stmcpp::clock::domain::domain domain(stmcpp::clock::domain::d1cpre::div1, stmcpp::clock::domain::d1ppre::div2, stmcpp::clock::domain::hpre::div2, stmcpp::clock::domain::d2ppre1::div2, stmcpp::clock::domain::d2ppre2::div2, stmcpp::clock::domain::d3ppre::div2, stmcpp::clock::domain::source::pll1);
 
-	//Set up the flash latency
-	constexpr std::uint32_t FLASH_ACR_CFG = (FLASH_ACR_LATENCY_4WS | (0b11 << FLASH_ACR_WRHIGHFREQ_Pos));
-	stmcpp::reg::write(std::ref(FLASH->ACR), FLASH_ACR_CFG);
-	while(stmcpp::reg::read(std::ref(FLASH->ACR)) != FLASH_ACR_CFG){}
+		//Wait for the MCU to switch to PLL1 as the clock source
+		stmcpp::clock::systick::waitForTrue(domain.sourceStatus() == stmcpp::clock::domain::source::pll1, []() { errorHandler.hardThrow(clock::error::domain_source_switch_timeout); });
 
-	//Setup the PLL (M=4 N=48 P=2 Q=16 R=2) -> sysclock = 384MHz
+		// Enable clock compensation cell and wait for it to be ready
+		stmcpp::reg::set(std::ref(SYSCFG->CCCSR), SYSCFG_CCCSR_EN);
+		stmcpp::reg::waitForBitSet(std::ref(SYSCFG->CCCSR), SYSCFG_CCCSR_READY_Msk, []() { errorHandler.hardThrow(clock::error::clock_compensation_timeout); });
 
-	stmcpp::clock::pll::setSource(stmcpp::clock::pll::clkSource::hse);
-	stmcpp::clock::pll::pll<stmcpp::clock::pll::peripheral::pll1, 3, 60, 2, 4, 2> pll1(stmcpp::clock::pll::inputRange::f8_16MHz);
-	pll1.enable();
-	while(!pll1.isLocked()){;}
+		// Select HSI48 as clock for the usb peripheral
+		static constexpr std::uint32_t usbClkSel = 0b11 << RCC_D2CCIP2R_USBSEL_Pos; 
+		stmcpp::reg::set(std::ref(RCC->D2CCIP2R), usbClkSel);
+		stmcpp::reg::waitForBitsEqual(std::ref(RCC->D2CCIP2R), RCC_D2CCIP2R_USBSEL_Msk, usbClkSel, []() { errorHandler.hardThrow(clock::error::clock_mux_timeout); });
 
+		//Select CSI as clock for loww speed peripherals (I2C, UART)
+		static constexpr std::uint32_t i2c123ClkSel = 0b01 << RCC_D2CCIP2R_I2C123SEL_Pos; 
+		stmcpp::reg::set(std::ref(RCC->D2CCIP2R), i2c123ClkSel);
+		stmcpp::reg::waitForBitsEqual(std::ref(RCC->D2CCIP2R), RCC_D2CCIP2R_I2C123SEL_Msk, i2c123ClkSel, []() { errorHandler.hardThrow(clock::error::clock_mux_timeout); });
 
-	stmcpp::clock::pll::pll<stmcpp::clock::pll::peripheral::pll2, 3, 50, 4, 4, 4> pll2(stmcpp::clock::pll::inputRange::f8_16MHz);
-	pll2.enable();
-	while(!pll2.isLocked()){;}
+		static constexpr std::uint32_t i2c4ClkSel = 0b01 << RCC_D3CCIPR_I2C4SEL_Pos; 
+		stmcpp::reg::set(std::ref(RCC->D3CCIPR), i2c4ClkSel);
+		stmcpp::reg::waitForBitsEqual(std::ref(RCC->D3CCIPR), RCC_D3CCIPR_I2C4SEL_Msk, i2c4ClkSel, []() { errorHandler.hardThrow(clock::error::clock_mux_timeout); });
 
-	//Setup the PLL for the USB
-	stmcpp::clock::pll::pll<stmcpp::clock::pll::peripheral::pll3, 8, 32, 2, 4, 12> pll3(stmcpp::clock::pll::inputRange::f8_16MHz);
-	pll3.enable();
-	while(!pll3.isLocked()){;}
-
-	//Setup the domain dividers
-	stmcpp::clock::domain::domain domain(stmcpp::clock::domain::d1cpre::div1, stmcpp::clock::domain::d1ppre::div2, stmcpp::clock::domain::hpre::div2, stmcpp::clock::domain::d2ppre1::div2, stmcpp::clock::domain::d2ppre2::div2, stmcpp::clock::domain::d3ppre::div2, stmcpp::clock::domain::source::pll1);
-
-	//Wait for the MCU to switch to PLL1 as the clock source
-	while(domain.sourceStatus() != stmcpp::clock::domain::source::pll1){;}
-
-	// Enable CSI (for clock compensation)
-	stmcpp::reg::set(std::ref(RCC->CR), RCC_CR_CSION);
-    while(!(stmcpp::reg::read(std::ref(RCC->CR)) & RCC_CR_CSION_Msk)){;}
-
-	// Enable clock compensation cell and wait for it to be ready
-	stmcpp::reg::set(std::ref(SYSCFG->CCCSR), SYSCFG_CCCSR_EN);
-	while(!(stmcpp::reg::read(std::ref(SYSCFG->CCCSR)) & SYSCFG_CCCSR_READY_Msk)){;}
-
-	// Select HSI48 as clock for the usb peripheral
-    stmcpp::reg::set(std::ref(RCC->D2CCIP2R), 0b11, RCC_D2CCIP2R_USBSEL_Pos);
-	while(stmcpp::reg::read(std::ref(RCC->D2CCIP2R), 0b11, RCC_D2CCIP2R_USBSEL_Pos) != 0b11){;}
-
-	// Select PLL2P as clock for the SPI1, 2 and 3
-    stmcpp::reg::set(std::ref(RCC->D2CCIP1R), 0b001, RCC_D2CCIP1R_SPI123SEL_Pos);
-	while(stmcpp::reg::read(std::ref(RCC->D2CCIP1R), 0b11, RCC_D2CCIP1R_SPI123SEL_Pos) != 0b001){;}
-
-	//Select PLL3R as clock for I2C peripherals
-	stmcpp::reg::set(std::ref(RCC->D2CCIP2R), 0b01, RCC_D2CCIP2R_I2C123SEL_Pos);
-	while(stmcpp::reg::read(std::ref(RCC->D2CCIP2R), 0b11, RCC_D2CCIP2R_I2C123SEL_Pos) != 0b01){;}
-	stmcpp::reg::set(std::ref(RCC->D3CCIPR), 0b01, RCC_D3CCIPR_I2C4SEL_Pos);
-	while(stmcpp::reg::read(std::ref(RCC->D3CCIPR), 0b11, RCC_D3CCIPR_I2C4SEL_Pos) != 0b01){;}
-	
+		static constexpr std::uint32_t usart234578ClkSel = 0b100 << RCC_D2CCIP2R_USART28SEL_Pos; 
+		stmcpp::reg::set(std::ref(RCC->D2CCIP2R), usart234578ClkSel);
+		stmcpp::reg::waitForBitsEqual(std::ref(RCC->D3CCIPR), RCC_D2CCIP2R_USART28SEL_Msk, usart234578ClkSel, []() { errorHandler.hardThrow(clock::error::clock_mux_timeout); });
+		
+	}
 }
