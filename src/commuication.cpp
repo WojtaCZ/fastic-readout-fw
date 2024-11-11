7683beb3e0813aa43e0aabbe8ec02e8035cac759
Vojtěch Vosáhlo <vosahlov@gmail.com>
2024-11-11 14:48:35 +0100

    Added a bit of PID

---
+++ src/commuication.cpp
@@ -0,0 +1,20 @@
+/*
+
+set fastic [1/2/both] register [add] [val] [force]
+get fastic [1/2/both] register [add] "get fastic (?:1|2|both) register 0x(?:[a-f0-9]){2}"
+
+
+
+get bias voltage
+set bias voltage [val]
+get bias current
+
+get readout info
+get readout config
+
+get userboard info
+get userboard config
+set userboard config
+
+*/
+
